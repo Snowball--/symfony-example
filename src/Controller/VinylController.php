c1d261af2cfe58d47e0a0ee41059b3f2e19d3415
Snowball <snow-snowball@yandex.ru>
2023-01-27 02:29:13 +0400

    Play with Twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
@@ -11,12 +12,42 @@ use function Symfony\Component\String\u;
 /**
  * VinylController
  */
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: some test title');
+        $tracks = [
+            [
+                'song' => 'Song 1',
+                'artist' => 'Artist 1',
+            ],
+            [
+                'song' => 'Song 2',
+                'artist' => 'Artist 2',
+            ],
+            [
+                'song' => 'Song 3',
+                'artist' => 'Artist 3',
+            ],
+            [
+                'song' => 'Song 4',
+                'artist' => 'Artist 4',
+            ],
+            [
+                'song' => 'Song 5',
+                'artist' => 'Artist 5',
+            ],
+            [
+                'song' => 'Song 6',
+                'artist' => 'Artist 6',
+            ]
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'Some test title',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
