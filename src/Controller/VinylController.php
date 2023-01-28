15beceb8aa8a2f8ac855a21afa07964fb765539b
Snowball <snow-snowball@yandex.ru>
2023-01-29 03:48:33 +0400

    Adding HttpClient, cache and external repo

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Knp\Bundle\TimeBundle\DateTimeFormatter;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use function Symfony\Component\String\u;
 
 /**
@@ -59,12 +63,23 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(?string $slug = null): Response
+    public function browse(HttpClientInterface $httpClient, CacheInterface $cache, ?string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
+        $mixes = $cache->get('mixes_data', function (CacheItemInterface $cacheItem) use ($httpClient) {
+            $cacheItem->expiresAfter(5);
+            $response = $httpClient->request(
+                'GET',
+                'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json'
+            );
+
+            return $response->toArray();
+        });
+
         return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre
+            'genre' => $genre,
+            'mixes' => $mixes
         ]);
     }
 }
