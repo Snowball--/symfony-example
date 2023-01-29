8417119b07a35150dfdd7fd79ff3a01ec3c432c6
Snowball <snow-snowball@yandex.ru>
2023-01-29 15:16:43 +0400

    Adding named HttpClient

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -7,6 +7,7 @@ namespace App\Service;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class MixRepository
 {
@@ -16,7 +17,8 @@ class MixRepository
      */
     public function __construct(
         private readonly CacheInterface $cache,
-        private readonly HttpClientInterface $client,
+        private readonly HttpClientInterface $githubContentClient,
+        #[Autowire('%kernel.debug%')]
         private readonly bool $isDebug
     ) {}
 
@@ -28,9 +30,9 @@ class MixRepository
     {
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-            $response = $this->client->request(
+            $response = $this->githubContentClient->request(
                 'GET',
-                'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json'
+                '/SymfonyCasts/vinyl-mixes/main/mixes.json'
             );
 
             return $response->toArray();
