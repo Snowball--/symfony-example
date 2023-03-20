547e628941cf4e9573a4aaabd50d6a23ed2d8231
Snowball <snow-snowball@yandex.ru>
2023-03-20 23:26:47 +0400

    Added view single mix action

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,5 +30,19 @@ class MixController extends AbstractController
             $mix->getId(),
             $mix->getTrackCount()
         ));
+    }
+
+    /**
+     * @param int $id
+     * @param VinylMixRepository $mixRepository
+     * @return Response
+     */
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show(int $id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix
+        ]);
     }
 }
